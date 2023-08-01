85adb7f0dec79f2457673268509edd2b8bd4d323
kerim <yoatomadd@gmail.com>
2023-08-01 02:17:54 +0200

    Update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,35 +1,33 @@
-
-
 function telegramIt() {
+    // Combine the form fields into a single `message` variable
+    require('dotenv').config();
+    console.log(process.env);
+    const name = document.getElementById("name").value;
+    const surname = document.getElementById("surname").value;
+    const age = document.getElementById("age").value;
+    const email = document.getElementById("email").value;
+    const telegram_acc = document.getElementById("telegram_acc").value;
+    const message = document.getElementById("message").value;
+    const text = `Name: ${name}\nSurname: ${surname}\nAge: ${age}\nEmail: ${email}\nTelegram Account: ${telegram_acc}\nMessage: ${message}`;
 
-        // Combine the form fields into a single `message` variable
-        const name = document.getElementById("name").value;
-        const surname = document.getElementById("surname").value;
-        const age = document.getElementById("age").value;
-        const email = document.getElementById("email").value;
-        const telegram_acc = document.getElementById("telegram_acc").value;
-        const message = document.getElementById("message").value;
-        const text = `Name: ${name}\nSurname: ${surname}\nAge: ${age}\nEmail: ${email}\nTelegram Account: ${telegram_acc}\nMessage: ${message}`;
-        let bot_id = process.env.BOT_IT
-        console.log(text)
-        // Perform the AJAX request using Fetch API
-        fetch(`https://api.telegram.org/bot${bot_id}/sendMessage?chat_id=@yeqanaL&text=${encodeURIComponent(text)}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                return response.json();
-            })
-            .then(data => {
-                // Handle the response from the server (if needed)
-                console.log("Server response:", data);
+    // Perform the AJAX request using Fetch API
+    fetch(`https://api.telegram.org/bot${process.env.API_KEY}/sendMessage?chat_id=@yeqanaL&text=${encodeURIComponent(text)}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Handle the response from the server (if needed)
+            console.log("Server response:", data);
 
-                // Show the "Thank you" message below the form
-                const thankYouMessage = document.getElementById("thankYouMessage");
-                thankYouMessage.style.display = "block";
-            })
-            .catch(error => {
-                // Handle any errors that occurred during the AJAX request
-                console.error("Error:", error);
-            });
-    }
+            // Show the "Thank you" message below the form
+            const thankYouMessage = document.getElementById("thankYouMessage");
+            thankYouMessage.style.display = "block";
+        })
+        .catch(error => {
+            // Handle any errors that occurred during the AJAX request
+            console.error("Error:", error);
+        });
+}
