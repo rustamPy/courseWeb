f3ac9ed8e0cb2ef78cf8a0e22f89a0fe7e2bb24f
kerim <yoatomadd@gmail.com>
2023-09-05 22:37:28 +0200

    Cherry pick - 1

--- assets/js/video.js
+++ assets/js/video.js
@@ -18,7 +18,7 @@ function videoPopup() {
             }
         }
     });
- }
+}
 /*! Magnific Popup - v1.1.0 - 2016-02-20
 * http://dimsemenov.com/plugins/magnific-popup/
 * Copyright (c) 2016 Dmitry Semenov; */
